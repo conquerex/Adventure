f78bac36f92c4023adaae0d65cc34fd76472799c
손종국 <conquerex@gmail.com>
2016-11-15 17:30:48 +0900

    용사가 뷰 소환술을 습득하였다.

--- Chap02_ViewForest_View/app/src/main/java/com/example/jongkook/chap02_viewforest_view/MainActivity.java
+++ Chap02_ViewForest_View/app/src/main/java/com/example/jongkook/chap02_viewforest_view/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    Button button2;
     EditText editText;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         // 버튼뷰 정의
         button = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+        img = (ImageView)findViewById(R.id.imageView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // 버튼을 클릭할 때마다 이미지뷰가 사라지고 나타남이 반복된다
+        // layout_alignWithParentIfMissing 실습
+        // 이미지가 사라질 경우 텍스트뷰는 부모뷰 기준으로 배치된다
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(img.getVisibility() == View.VISIBLE){
+                    img.setVisibility(View.GONE);
+                }else{
+                    img.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
