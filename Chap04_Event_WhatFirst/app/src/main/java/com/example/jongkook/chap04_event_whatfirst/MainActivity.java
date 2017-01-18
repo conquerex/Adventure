eaf7e99bbf11d0a1165ce2078084a5d65ae4ee73
손종국 <conquerex@gmail.com>
2017-01-18 19:42:16 +0900

    용사는 요리할 때, 외부 변수를 조미료로 썼다.

--- Chap04_Event_WhatFirst/app/src/main/java/com/example/jongkook/chap04_event_whatfirst/MainActivity.java
+++ Chap04_Event_WhatFirst/app/src/main/java/com/example/jongkook/chap04_event_whatfirst/MainActivity.java
@@ -5,12 +5,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**  외부 변수 액세스 실습 코드 - 시작  **/
+        setContentView(R.layout.activity_main);
+        LinearLayout linear = (LinearLayout)findViewById(R.id.activity_main);
+        tv = (TextView)findViewById(R.id.tvText);
+
+        // onCreate 내에 정의된 내부클래스(OnTouchListener의 익명 서브클래스)는
+        // final이 아닌 변수를 참조할 수 없다.
+        // outText는 onCreate가 리턴되면 사라지는 지역변수
+        // 하지만 onTouch는 이벤트 발생시 언제든지 호출가능.
+        // 상수(final)가 되어 onTouch를 등록하는 시점에 그 값을 전달
+        // ** 리스너로 전달할 지역 변수는 final로 선언한다
+        final TextView outText = (TextView)findViewById(R.id.tvText);
+
+        linear.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            // onTouch 메서드는 onCreate에 속한 지역 메서드가 아님
+            // 터치 이벤트 발생시 실행할 메서드로 등록만 할
+            // onTouch 메서드가 속한 객체는 익명의 리스너 타입
+            // onCreate 메서드 내에 선언된 로컬 내부 클래스
+            //  - 내부클래스는 외부 클래스와 긴밀하게 연관. 외부 클래스의 멤버를 참조할 수 있다
+            //  - 리스너 내에서 액티비티의 findviewById
+            //  - 텍스트뷰 객체를 따로 선언해도 된다. 더 효율적. 매번 검새갈 필요 없음.
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    // TextView tv = (TextView)findViewById(R.id.tvText);
+                    outText.setText("Touched");
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        /**  외부 변수 액세스 실습 코드 - 끝  **/
+
+
         View view = new MyView(this);
         // 리스너 - 1순위
         view.setOnTouchListener(new View.OnTouchListener(){
@@ -27,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        setContentView(view);
+        /**  외부 변수 액세스 실습 코드 - 아래 주석처리  **/
+        // setContentView(view);
     }
 
     protected class MyView extends View{
