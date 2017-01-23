52d0a72f8167b4d4ff93ec96e6698919ad1b375f
손종국 <conquerex@gmail.com>
2017-01-23 23:59:43 +0900

    용사는 Touch 구이를 완성하였다.

--- Chap04_Event_SeveralEvent/app/src/main/java/com/example/jongkook/chap04_event_severalevent/MainActivity.java
+++ Chap04_Event_SeveralEvent/app/src/main/java/com/example/jongkook/chap04_event_severalevent/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -13,9 +14,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Myview view;
+
+    // Vertex 클래스
     // 정점 하나에 대한 정보를 가지는 클래스
     public class Vertex{
         float x, y;
+        // draw : 선을 그을 것인지 이동만 할 것인지.
         boolean draw;
         public Vertex(float ax, float ay, boolean ad) {
             x = ax;
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 곡선을 구성하는 점의 갯수는 제한이 없으므로 고정크기 배열 사용 X
+    // 추가만 되고 삽입, 삭제 없어 --> ArrayList
     ArrayList<Vertex> arVertex;
 
     @Override
@@ -44,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             paint = new Paint();
             paint.setColor(Color.BLUE);
             paint.setStrokeWidth(3);
+            // setAntiAlias : 부드럽게 그리기 위함
             paint.setAntiAlias(true);
         }
 
@@ -52,14 +59,18 @@ public class MainActivity extends AppCompatActivity {
             canvas.drawColor(Color.LTGRAY);
 
             // 정점을 순회하면서 선분으로 잇는다
+            // i = 0 일때는 draw 멤버가 항상 false
             for(int i = 0;i<arVertex.size();i++){
                 if(arVertex.get(i).draw){
+                    Log.i("onDraw","----- "+i);
                     canvas.drawLine(arVertex.get(i-1).x, arVertex.get(i-1).y
                         ,arVertex.get(i).x, arVertex.get(i).y, paint);
                 }
             }
         }
 
+        // 뷰를 터치하거나 이동할 때 발생하는 터치 이벤트
+        // draw 멤버 변수 구분을 위한 이벤트
         @Override
         public boolean onTouchEvent(MotionEvent event) {
             if(event.getAction() == MotionEvent.ACTION_DOWN){
@@ -68,7 +79,10 @@ public class MainActivity extends AppCompatActivity {
             }
             if(event.getAction() == MotionEvent.ACTION_MOVE){
                 arVertex.add(new Vertex(event.getX(), event.getY(), true));
+                // invalidate : 새로 추가된 선을 출력하기 위함
+                // 전체 뷰를 무효 - 화면을 갱신
                 invalidate();
+                // 직후 onDraw 호출
                 return true;
             }
             return false;
